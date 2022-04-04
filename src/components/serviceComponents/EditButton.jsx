cc82139ce290beef8b467099e30acd4efcbf54ec
mickael <mickael@soloso.fr>
2022-04-04 14:32:39 +0300

    [EditButton] Add translation

--- src/components/serviceComponents/EditButton.jsx
+++ src/components/serviceComponents/EditButton.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
 import { Flex, Image, Text, useDisclosure } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
 
 import imgSource from "../assets/images/Vector.png";
 
 const EditButton = () => {
   const { onOpen } = useDisclosure();
+  const { t } = useTranslation();
 
   return (
     <Flex align="center">
       <Image onClick={onOpen} src={imgSource} alt="image" mr="1" />
       <Text onClick={onOpen} className="edit-txt">
-        edit
+        {t("startup.popup.service.details.content.edit.text")}
       </Text>
     </Flex>
   );
