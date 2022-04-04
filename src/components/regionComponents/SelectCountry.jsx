18bb1d7ccbcb1fff519e31da4de63ab317e94bfd
mickael <mickael@soloso.fr>
2022-04-04 14:26:10 +0300

    [SelectCountry] Add translation

--- src/components/regionComponents/SelectCountry.jsx
+++ src/components/regionComponents/SelectCountry.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
-import { Select, Box } from "@chakra-ui/react";
+import { Box, Select } from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
 
 const SelectCountry = (props) => {
+  const { t } = useTranslation();
+
   return (
     <Box>
       <Select
         size="sm"
         disabled={props.data.length <= 0}
-        placeholder="Select Country"
+        placeholder={t("mapping.popup.component.placeholder.country.selection")}
         value={props.country}
         onChange={(e) => {
           props.selectedCountry(e.target.value);
